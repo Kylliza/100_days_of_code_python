60a905326cbd57a290934d7ee5207acd088d846d
Kylliza <alexisavila6478@gmail.com>
2025-03-31 21:15:40 -0600

    day 2 100 days of code: python

--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,11 @@
-len(12345)
+print(len("12345"))
+
+print(type("Hi"))
+print(type(1))
+print(type(True))
+print(type(2.3))
+
+name = (input("Enter your name"))
+length = len(name)
+
+print("Number of letters in your name: " + str(length))
